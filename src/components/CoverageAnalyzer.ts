dbc8923616421a88f9929a89c5242ef7e667271d
Hossein Soltanloo <hossein.soltanloo@gmail.com>
2024-03-07 12:07:52 -0800

    Add mock output data to AsyncCoverageAnalyzer

--- src/components/CoverageAnalyzer.ts
+++ src/components/CoverageAnalyzer.ts
@@ -1,39 +1,61 @@
 type Location = `${string}:${string}:${string}:${string}:${string}`;
 type PromiseType =
-  | "NewPromise"
-  | "AsyncFunction"
-  | "PromiseThen"
-  | "PromiseCatch"
-  | "PromiseResolve"
-  | "PromiseReject"
-  | "PromiseAll"
-  | "PromiseRace";
+    | "NewPromise"
+    | "AsyncFunction"
+    | "PromiseThen"
+    | "PromiseCatch"
+    | "PromiseResolve"
+    | "PromiseReject"
+    | "PromiseAll"
+    | "PromiseRace";
 type PromiseIdentifier = number;
 
 export interface PromiseCoverageWarnings {
-  fulfillment?: boolean;
-  rejection?: boolean;
-  fulfillReactionRegistration?: boolean;
-  rejectReactionRegistration?: boolean;
-  fulfillReactionExecution?: boolean;
-  rejectReactionExecution?: boolean;
+    fulfillment?: boolean;
+    rejection?: boolean;
+    fulfillReactionRegistration?: boolean;
+    rejectReactionRegistration?: boolean;
+    fulfillReactionExecution?: boolean;
+    rejectReactionExecution?: boolean;
 }
 
-export interface PromiseCoverageInfo {
-  identifier: PromiseIdentifier;
-  location: Location;
-  type: PromiseType;
-  warnings: PromiseCoverageWarnings;
-  parent: PromiseIdentifier;
-  code: string;
+export interface PromiseInfo {
+    identifier: PromiseIdentifier;
+    location: Location;
+    type: PromiseType;
+    warnings: PromiseCoverageWarnings;
+    parent?: PromiseIdentifier;
+    code: string;
 }
 
-const mockPromiseCoverageData: PromiseCoverageInfo[] = [];
+type PromiseCoverageReport = Array<PromiseInfo>;
+
+const mockPromiseCoverageData: PromiseCoverageReport = [
+    {
+        identifier: 116,
+        location: "path/to/file.js:12:5:17:20",
+        type: "NewPromise",
+        warnings: {
+            rejection: true,
+        },
+        code: `new Promise((resolve, reject) => {
+                    if (num > 10) {
+                        resolve("The number is greater than 10!");
+                    } else {
+                        reject("The number is not greater than 10.");
+                    }
+                });`
+    }
+];
 
 class AsyncCoverageAnalyzer {
-  private coverageData?: PromiseCoverageInfo[];
+    private coverageData?: PromiseCoverageReport;
 
-  constructor() {}
+    constructor(projectPath: string) {
+    }
 
-  public analyze(): void {}
+    public analyze(): PromiseCoverageReport {
+        // Run JScope on the specified projectPath, process the output, and return the results
+        return mockPromiseCoverageData
+    }
 }
