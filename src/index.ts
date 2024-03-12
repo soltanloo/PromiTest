91d846d5715ae2dcac7b3879de04d384ef8acb2e
Hossein Soltanloo <hossein.soltanloo@gmail.com>
2024-03-12 15:25:10 -0700

    Rename OpenAI component to GPTController

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-import { GPTController } from './components/OpenAI';
+import dotenv from "dotenv";
+import { GPTController } from "./components/GPTController";
+
 dotenv.config();
 
-
 async function main() {
-  console.log(await GPTController.getInstance().ask('Say this is a test'));
+  console.log(await GPTController.getInstance().ask("Say this is a test"));
 }
 
 main();
