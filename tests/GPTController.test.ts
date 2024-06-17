eaa20e7dd7756e72ae1169f6f8af685ea00dc6ff
Hossein Soltanloo <hossein.soltanloo@gmail.com>
2024-06-17 14:52:09 -0700

    Increase test timeout for GPTController tests

--- tests/GPTController.test.ts
+++ tests/GPTController.test.ts
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
-import {GPTController} from "../src/components/GPTController";
+import {GPTController} from "../src/components/apis/GPTController";
 import {assert} from "chai";
 
 dotenv.config();
 
 describe("GPT Controller", function () {
     it('should respond correctly to "Say this is a test"', function (done) {
-        this.timeout(30000);
+        this.timeout(100000);
         GPTController.getInstance()
             .ask("Say this is a test")
             .then((response) => {
