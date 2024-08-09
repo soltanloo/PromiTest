319b52e802ecded16ad95eee94108131bb6bd326
lsa125 <lsa125@DESKTOP-9SS3OM3>
2024-08-09 16:28:22 -0700

    added more logs, removed all instances of var from added code, changed logger to use a variable set in .env, added winston as a dependency

--- src/components/candidate-promise-finder/RootNodeMarkingStrategy.ts
+++ src/components/candidate-promise-finder/RootNodeMarkingStrategy.ts
@@ -1,22 +1,31 @@
 import {NodeMarkingStrategy} from "./NodeMarkingStrategy";
 import {PromiseNode} from "../promise-graph/PromiseNode";
 import {isPromiseCalling} from "../../utils/AST";
-import logger from "../../logging/logger";
+import logger from "../../utils/logger";
 
 export class RootNodeMarkingStrategy implements NodeMarkingStrategy {
     public markNode(node: PromiseNode): void {
+        logger.info(`Marking node ${node.id} as root node.`);
+
+        let _isRejectable = this.isRejectable(node);
+        let _isResolvable = this.isResolvable(node);
+
+        logger.debug(`Warnings: ${JSON.stringify(node.promiseInfo.warnings)}, isRejectable: ${_isRejectable}, isResolvable: ${_isResolvable}`);
+
         if (node.promiseInfo.warnings.rejection && this.isRejectable(node)) {
+            logger.info(`Node ${node.id} is flagged as rejectable.`);
             node.flags.rejectable = true;
         }
         if (node.promiseInfo.warnings.fulfillment && this.isResolvable(node)) {
+            logger.info(`Node ${node.id} is flagged as resolvable.`);
             node.flags.fulfillable = true;
         }
     }
 
     private isRejectable(node: PromiseNode): boolean {
-        var out = isPromiseCalling(node.promiseInfo.code, "reject")
-        logger.debug("isRejectable", {message: out})
-        return out;
+        let isPromiseCallingResult = isPromiseCalling(node.promiseInfo.code, "reject")
+        logger.debug("isRejectable", {message: isPromiseCallingResult})
+        return isPromiseCallingResult;
         // if (node.promiseInfo.type === "NewPromise") {
         //     const rejectablePatterns = [/reject\(/, /throw /];
         //     return rejectablePatterns.some(pattern => pattern.test(node.promiseInfo.code));
@@ -29,9 +38,9 @@ export class RootNodeMarkingStrategy implements NodeMarkingStrategy {
 
 
     private isResolvable(node: PromiseNode): boolean {
-        var out = isPromiseCalling(node.promiseInfo.code, "resolve")
-        logger.debug("isResolvable", {message: out})
-        return out;
+        let isPromiseCallingResult = isPromiseCalling(node.promiseInfo.code, "resolve")
+        logger.debug("isResolvable", {message: isPromiseCallingResult})
+        return isPromiseCallingResult;
         // if (node.promiseInfo.type === "NewPromise") {
         //     const resolvablePatterns = [/resolve\(/];
         //     return resolvablePatterns.some(pattern => pattern.test(node.promiseInfo.code));
--- src/components/configuration/RuntimeConfig.ts
+++ src/components/configuration/RuntimeConfig.ts
@@ -1,9 +1,8 @@
-import * as fs from "fs"
-import * as path from "path"
-import {Configuration} from "../../types/Configuration.type";
-import {PROMITEST_CONFIG_FILE_NAME} from "../../constants/constants";
-import logger from "../../logging/logger";
-
+import * as fs from "fs";
+import * as path from "path";
+import { Configuration } from "../../types/Configuration.type";
+import { PROMITEST_CONFIG_FILE_NAME } from "../../constants/constants";
+import logger from "../../utils/logger";
 
 export default class RuntimeConfig {
     private static instance: RuntimeConfig;
@@ -12,23 +11,29 @@ export default class RuntimeConfig {
 
     constructor(projectPath: string) {
         try {
+            logger.debug(`Initializing RuntimeConfig with projectPath: ${projectPath}`);
             this.originalProjectPath = projectPath;
             projectPath += projectPath.endsWith("/") ? "" : "/";
-            const rc = JSON.parse(fs.readFileSync(path.join(projectPath, PROMITEST_CONFIG_FILE_NAME), 'utf-8'))
+            logger.debug(`Adjusted projectPath: ${projectPath}`);
+            
+            const configFilePath = path.join(projectPath, PROMITEST_CONFIG_FILE_NAME);
+            logger.debug(`Reading config file from: ${configFilePath}`);
+            const rc = JSON.parse(fs.readFileSync(configFilePath, 'utf-8'));
+            
             let projectName = projectPath.split("/").slice(-2, -1)[0]; // To handle trailing slashes
-            logger.info(`Project Name: ${projectName}`);
-            logger.info(`Project Path: ${projectPath}`);
-            logger.info(`Config File: ${JSON.stringify(rc, null, 2)}`);
+            logger.debug(`Extracted projectName: ${projectName}`);
             this._config = {
                 projectPath,
                 projectName,
                 ...rc
-            }
+            };
+            logger.info(`Successfully loaded project configuration for project: ${projectName}`);
         } catch (e) {
-            logger.error('Could not parse config file:', {message: e});
+            logger.error('Could not parse config file:', { message: e });
             throw new Error('Could not parse config file.');
         }
-        console.log(`Project Configuration: ${JSON.stringify(this.config, null, 2)}`)
+
+        logger.info(`Project Configuration: ${JSON.stringify(this.config, null, 2)}`);
     }
 
     get config(): Configuration {
@@ -37,14 +42,19 @@ export default class RuntimeConfig {
 
     public static getInstance(projectPath?: string): RuntimeConfig {
         if (projectPath) {
+            logger.debug(`Retrieving instance of RuntimeConfig with projectPath: ${projectPath}`);
             if (RuntimeConfig?.instance?.originalProjectPath === projectPath) {
+                logger.debug(`Returning existing instance for projectPath: ${projectPath}`);
                 return RuntimeConfig.instance;
             } else {
+                logger.debug(`Creating new instance for projectPath: ${projectPath}`);
                 RuntimeConfig.instance = new RuntimeConfig(projectPath);
                 return RuntimeConfig.instance;
             }
         } else {
+            logger.debug(`Retrieving existing instance of RuntimeConfig`);
             if (!RuntimeConfig.instance) {
+                logger.error('RuntimeConfig is not instantiated and projectPath is missing');
                 throw new Error('RuntimeConfig is not instantiated and projectPath is missing');
             } else {
                 return RuntimeConfig.instance;
