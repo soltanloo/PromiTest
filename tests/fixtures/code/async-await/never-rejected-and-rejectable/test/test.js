ad58d82f0eb5bc05f81485357721d6448bfca024
Hossein Soltanloo <hossein.soltanloo@gmail.com>
2024-08-09 15:06:35 -0700

    Add a test case for async functions

---
+++ tests/fixtures/code/async-await/never-rejected-and-rejectable/test/test.js
@@ -0,0 +1,13 @@
+// test.js
+
+const assert = require('assert');
+const {
+	foo
+} = require('../index.js');
+
+describe('foo test', function () {
+	it('should resolve bar Promise', async function (done) {
+		await foo();
+		done()
+	})
+})
