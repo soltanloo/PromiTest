55dcf7b0c8c3b3e221f3c63492a7a71dcb4b6175
lsa125 <lsa125@DESKTOP-9SS3OM3>
2024-08-13 10:46:36 -0700

    promisegraph logs added

--- src/components/promise-graph/PromiseGraphConstructor.ts
+++ src/components/promise-graph/PromiseGraphConstructor.ts
@@ -1,6 +1,7 @@
 import {PromiseNode} from "./PromiseNode";
 import {PromiseGraph} from "./PromiseGraph";
 import {PromiseCoverageReport, PromiseIdentifier} from "../../types/CoverageAnalyzer.type";
+import logger from "src/utils/logger";
 
 export class PromiseGraphConstructor {
     promiseCoverageData: PromiseCoverageReport;
@@ -11,6 +12,7 @@ export class PromiseGraphConstructor {
     }
 
     public constructGraph(): PromiseGraph {
+        logger.info("Constructing promise graph");
         const nodeDirectory = new Map<PromiseIdentifier, PromiseNode>();
         this.promiseCoverageData.forEach((promise) => {
                 nodeDirectory.set(
@@ -21,7 +23,7 @@ export class PromiseGraphConstructor {
         );
 
         this.promiseGraph = new PromiseGraph(nodeDirectory);
-
+        logger.debug("Promise graph constructed successfully");
         return this.promiseGraph;
     }
 }
