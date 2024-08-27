b6f6aa41e42a945838d4f532ea1ce441a16fbce1
lsa125 <lsa125@DESKTOP-9SS3OM3>
2024-08-27 16:24:19 -0700

    Remove experimental changes

--- src/components/apis/GPTController.ts
+++ src/components/apis/GPTController.ts
@@ -3,6 +3,7 @@ import logger from '../../utils/logger';
 import { ThrowBypassSystemPrompt } from '../../prompt-templates/ThrowBypassSystemPrompt';
 import { LLMControllerInterface } from './LLMControllerInterface';
 export class GPTController implements LLMControllerInterface {
+
     private static instance: GPTController;
     private static apiInstance: OpenAI;
     private static readonly MAX_TOKENS = 1000;
@@ -15,7 +16,7 @@ export class GPTController implements LLMControllerInterface {
         logger.info('GPTController initialized with OpenAI API.');
     }
 
-    public static getInstance(): LLMControllerInterface {
+    public static getInstance(): GPTController {
         if (!GPTController.instance) {
             logger.debug('Creating new instance of GPTController.');
             GPTController.instance = new GPTController();
@@ -30,39 +31,7 @@ export class GPTController implements LLMControllerInterface {
     public static setModel(model: GPTController.Models) {
         this.model = model;
     }
-    public askWithMessages(userMessages: any): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const params: OpenAI.Chat.ChatCompletionCreateParams = {
-                messages: userMessages,
-                model: GPTController.model,
-                max_tokens: GPTController.MAX_TOKENS,
-            };
 
-            logger.debug(`Sending message to GPT model: \n ${userMessages}`);
-
-            GPTController.apiInstance.chat.completions
-                .create(params)
-                .then((res) => {
-                    const responseContent = res.choices[0]?.message?.content;
-
-                    if (!responseContent) {
-                        logger.error('No response received from GPT model.');
-                        reject(new Error('No response'));
-                    } else {
-                        logger.info(
-                            `Received response from GPT model: ${responseContent}`,
-                        );
-                        resolve(responseContent);
-                    }
-                })
-                .catch((err) => {
-                    logger.error(
-                        `Error occurred while communicating with GPT model. ${err.message}`,
-                    );
-                    reject(err);
-                });
-        });
-    }
     public ask(question: string): Promise<string> {
         return new Promise((resolve, reject) => {
             const params: OpenAI.Chat.ChatCompletionCreateParams = {
@@ -98,21 +67,19 @@ export class GPTController implements LLMControllerInterface {
     }
     public verifyThrowCanBeBypassed(functionCode: string): Promise<boolean> {
         return new Promise((resolve, reject) => {
+            
             const params: OpenAI.Chat.ChatCompletionCreateParams = {
                 messages: [
                     { role: 'system', content: ThrowBypassSystemPrompt },
-                    { role: 'user', content: functionCode },
+                    { role: 'user', content: functionCode }
                 ],
                 model: GPTController.model,
                 max_tokens: GPTController.MAX_TOKENS,
             };
 
-            logger.debug(
-                `Sending the following message to GPT model:\nsystem:\n ${ThrowBypassSystemPrompt}\nuser:\n ${functionCode}`,
-            );
+            logger.debug(`Sending the following message to GPT model:\nsystem:\n ${ThrowBypassSystemPrompt}\nuser:\n ${functionCode}`);
 
-            GPTController.apiInstance.chat.completions
-                .create(params)
+            GPTController.apiInstance.chat.completions.create(params)
                 .then((res) => {
                     const responseContent = res.choices[0]?.message?.content;
 
@@ -120,17 +87,12 @@ export class GPTController implements LLMControllerInterface {
                         logger.error('No response received from GPT model.');
                         reject(new Error('No response'));
                     } else {
-                        logger.info(
-                            `Received response from GPT model. Response: ${responseContent}`,
-                        );
+                        logger.info(`Received response from GPT model. Response: ${responseContent}`);
                         resolve(responseContent === 'T');
                     }
                 })
                 .catch((err) => {
-                    logger.error(
-                        'Error occurred while communicating with GPT model.',
-                        { error: err.message },
-                    );
+                    logger.error('Error occurred while communicating with GPT model.', { error: err.message });
                     reject(err);
                 });
         });
@@ -142,6 +104,6 @@ export namespace GPTController {
         GPT4O = 'gpt-4o',
         GPT4TURBO = 'gpt-4-turbo',
         GPT4 = 'gpt-4',
-        GPT35TURBO = 'gpt-3.5-turbo',
+        GPT35TURBO = 'gpt-3.5-turbo'
     }
 }
