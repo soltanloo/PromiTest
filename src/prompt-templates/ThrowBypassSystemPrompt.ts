35cd1baec47f3225ab145e9d0490971317568b84
lsa125 <lsa125@DESKTOP-9SS3OM3>
2024-08-27 15:56:19 -0700

    Change prompt to say Javascript async instead of typescript

--- src/prompt-templates/ThrowBypassSystemPrompt.ts
+++ src/prompt-templates/ThrowBypassSystemPrompt.ts
@@ -1,1 +1,1 @@
-export const ThrowBypassSystemPrompt = "You will receive an asynchronous typescript function. Reply 'T' if the error throwing of this function can be bypassed only by changing the arguments of the function. Reply 'F' if it cannot.";
+export const ThrowBypassSystemPrompt = "You will receive an Javascript async function. Reply 'T' if the error throwing of this function can be bypassed only by changing the arguments of the function. Reply 'F' if it cannot.";
